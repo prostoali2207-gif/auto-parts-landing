bf8d18711e133fda5d1dcd8f8dcc7e09bc1f5cb1
prostoali2207-gif <prostoali2207@gmail.com>
2026-08-13 10:45:40 +0400

    Load UI/UX pass 3 overrides

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { IBM_Plex_Mono, IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import "./brand.css";
 import "./ui-polish.css";
+import "./ui-pass-3.css";
 
 const plexSans = IBM_Plex_Sans({
   variable: "--font-plex-sans",
