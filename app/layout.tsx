c52f2fb61e6175dbf4d5f94b5fd3a3ad2f0f4e9c
prostoali2207-gif <prostoali2207@gmail.com>
2026-08-13 14:57:31 +0400

    Load Visual Reset V4 layer

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import "./ui-pass-3.css";
 import "./ui-pass-4.css";
 import "./ui-rebuild-v2.css";
 import "./ui-visual-refinement-v3.css";
+import "./ui-visual-reset-v4.css";
 
 const plexSans = IBM_Plex_Sans({
   variable: "--font-plex-sans",
